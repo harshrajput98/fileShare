252dbdbcad63acd36dd9c18d21afc0e780517efb
Harsh Kumar <116553146+harshrajput98@users.noreply.github.com>
2025-01-06 21:13:22 +0530

    Update emailTemplate.js

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -123,7 +123,10 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                                             <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
                                             <tbody>
                                                 <tr>
-                                                <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color:rgb(219, 77, 16); border-radius: 5px; text-align: center;"> <a href="${downloadLink}" target="_blank" style="display: inline-block; color: #ffffff; background-color: ##FFA500; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">Download file</a> </td>
+                                               <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: rgb(219, 77, 16); border-radius: 5px; text-align: center;">
+    <a href="${downloadLink}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #FFA500; border: solid 1px #FFA500; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize;">Download file</a>
+</td>
+
                                                 </tr>
                                             </tbody>
                                             </table>
